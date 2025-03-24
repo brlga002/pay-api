b1bd3b3a74a5ccdf675d1b2e31494a62549fc3a2
brlga002 <gabriel.lima1000@gmail.com>
2025-03-24 04:10:48 -0400

    feat: add Card entity with validation and Luhn check for credit card details

---
+++ src/domain/entities/card.entity.ts
@@ -0,0 +1,143 @@
+import { DomainError } from "@domain/exceptions/DomainError";
+import { PaymentMethod, PaymentType } from "./payment-method";
+
+interface CardProps {
+	number: string;
+	holderName: string;
+	cvv: string;
+	expirationDate: string;
+	installments: number;
+}
+
+export class Card extends PaymentMethod {
+	_number!: string;
+	_holderName!: string;
+	_cvv!: string;
+	_expirationDate!: string;
+	_installments!: number;
+
+	constructor(props: CardProps) {
+		super(PaymentType.CREDIT);
+		this.number = props.number;
+		this.holderName = props.holderName;
+		this.cvv = props.cvv;
+		this.expirationDate = props.expirationDate;
+		this.installments = props.installments;
+	}
+
+	get number() {
+		return this._number;
+	}
+
+	set number(value: string) {
+		const sanitizedValue = value.replace(/\s/g, "");
+		if (!/^\d+$/.test(sanitizedValue))
+			throw new DomainError("Invalid card number, must contain only digits");
+
+		if (sanitizedValue.length !== 16)
+			throw new DomainError("Invalid card number length, must be 16 digits");
+
+		if (!Card.luhnCheck(sanitizedValue))
+			throw new DomainError("Invalid card number, failed Luhn algorithm");
+
+		this._number = sanitizedValue;
+	}
+
+	get holderName() {
+		return this._holderName;
+	}
+
+	set holderName(value: string) {
+		if (!value || value === "")
+			throw new DomainError("Invalid holder name, must not be empty");
+		if (value.length < 3)
+			throw new DomainError(
+				"Invalid holder name, must contain at least 3 characters",
+			);
+
+		this._holderName = value;
+	}
+
+	get cvv() {
+		return this._cvv;
+	}
+
+	set cvv(value: string) {
+		const sanitizedValue = value.replace(/\s/g, "");
+		if (!/^\d+$/.test(sanitizedValue))
+			throw new DomainError("Invalid cvv, must contain only digits");
+
+		if (sanitizedValue.length !== 3)
+			throw new DomainError("Invalid cvv length, must be 3 digits");
+
+		this._cvv = sanitizedValue;
+	}
+
+	get expirationDate() {
+		return this._expirationDate;
+	}
+
+	set expirationDate(value: string) {
+		const sanitizedValue = value.replace(/\s/g, "");
+
+		if (!/^(\d{1,2})\/\d{4}$/.test(sanitizedValue))
+			throw new DomainError(
+				`Invalid expiration date format, must be MM/YYYY, received ${sanitizedValue}`,
+			);
+
+		const [month, year] = sanitizedValue.split("/").map(Number);
+
+		if (month < 1 || month > 12)
+			throw new DomainError(
+				"Invalid expiration month, must be between 1 and 12",
+			);
+
+		const expirationDate = new Date(year, month, 0);
+		const now = new Date();
+
+		if (expirationDate < now)
+			throw new DomainError("Card is expired", {
+				expirationDate: sanitizedValue,
+			});
+
+		this._expirationDate = `${String(month).padStart(2, "0")}/${year}`;
+	}
+
+	get installments() {
+		return this._installments;
+	}
+
+	set installments(value: number) {
+		if (value < 1)
+			throw new DomainError("Invalid installments, must be greater than 0");
+
+		this._installments = value;
+	}
+
+	static luhnCheck(value: string): boolean {
+		const digits = value.split("").map(Number);
+		const parity = digits.length % 2;
+
+		return (
+			digits.reduce((acc, digit, index) => {
+				let modifiedDigit = digit;
+				if (index % 2 === parity) modifiedDigit *= 2;
+				if (modifiedDigit > 9) modifiedDigit -= 9;
+				return acc + modifiedDigit;
+			}, 0) %
+				10 ===
+			0
+		);
+	}
+
+	toJSON() {
+		return {
+			...super.toJSON(),
+			number: this.number,
+			holderName: this.holderName,
+			cvv: this.cvv,
+			expirationDate: this.expirationDate,
+			installments: this._installments,
+		};
+	}
+}
---
+++ src/domain/entities/card.spec.ts
@@ -0,0 +1,187 @@
+import { Card } from "@domain/entities/card.entity";
+
+describe("Card", () => {
+	it("should create a valid card with all correct fields", () => {
+		const card = new Card({
+			number: "4111111111111111",
+			holderName: "João da Silva",
+			cvv: "123",
+			expirationDate: "12/2099",
+			installments: 3,
+		});
+
+		expect(card.number).toBe("4111111111111111");
+		expect(card.holderName).toBe("João da Silva");
+		expect(card.cvv).toBe("123");
+		expect(card.expirationDate).toBe("12/2099");
+		expect(card.installments).toBe(3);
+	});
+
+	it("should create a valid card with all correct fields with a single digit month", () => {
+		const card = new Card({
+			number: "4111111111111111",
+			holderName: "João da Silva",
+			cvv: "123",
+			expirationDate: "1/2099",
+			installments: 3,
+		});
+
+		expect(card.number).toBe("4111111111111111");
+		expect(card.holderName).toBe("João da Silva");
+		expect(card.cvv).toBe("123");
+		expect(card.expirationDate).toBe("01/2099");
+		expect(card.installments).toBe(3);
+	});
+
+	it("should throw an error if the card number contains letters", () => {
+		expect(() => {
+			new Card({
+				number: "4111 1111 1111 abcd",
+				holderName: "João",
+				cvv: "123",
+				expirationDate: "12/2099",
+				installments: 1,
+			});
+		}).toThrow("Invalid card number, must contain only digits");
+	});
+
+	it("should throw an error if the card number is not 16 digits long", () => {
+		expect(() => {
+			new Card({
+				number: "411111111111",
+				holderName: "João",
+				cvv: "123",
+				expirationDate: "12/2099",
+				installments: 1,
+			});
+		}).toThrow("Invalid card number length, must be 16 digits");
+	});
+
+	it("should throw an error if the card number fails the Luhn algorithm", () => {
+		expect(() => {
+			new Card({
+				number: "1234567812345678",
+				holderName: "João",
+				cvv: "123",
+				expirationDate: "12/2099",
+				installments: 1,
+			});
+		}).toThrow("Invalid card number, failed Luhn algorithm");
+	});
+
+	it("should throw an error if the holder name is empty", () => {
+		expect(() => {
+			new Card({
+				number: "4111111111111111",
+				holderName: "",
+				cvv: "123",
+				expirationDate: "12/2099",
+				installments: 1,
+			});
+		}).toThrow("Invalid holder name, must not be empty");
+	});
+
+	it("should throw an error if the holder name is less than 3 characters", () => {
+		expect(() => {
+			new Card({
+				number: "4111111111111111",
+				holderName: "Jo",
+				cvv: "123",
+				expirationDate: "12/2099",
+				installments: 1,
+			});
+		}).toThrow("Invalid holder name, must contain at least 3 characters");
+	});
+
+	it("should throw an error if the CVV contains non-digit characters", () => {
+		expect(() => {
+			new Card({
+				number: "4111111111111111",
+				holderName: "João",
+				cvv: "12a",
+				expirationDate: "12/2099",
+				installments: 1,
+			});
+		}).toThrow("Invalid cvv, must contain only digits");
+	});
+
+	it("should throw an error if the CVV is not 3 digits long", () => {
+		expect(() => {
+			new Card({
+				number: "4111111111111111",
+				holderName: "João",
+				cvv: "12",
+				expirationDate: "12/2099",
+				installments: 1,
+			});
+		}).toThrow("Invalid cvv length, must be 3 digits");
+	});
+
+	it("should throw an error if the expiration date has an invalid format", () => {
+		expect(() => {
+			new Card({
+				number: "4111111111111111",
+				holderName: "João",
+				cvv: "123",
+				expirationDate: "2099-12",
+				installments: 1,
+			});
+		}).toThrow("Invalid expiration date format, must be MM/YYYY");
+	});
+
+	it("should throw an error if the expiration month is invalid", () => {
+		expect(() => {
+			new Card({
+				number: "4111111111111111",
+				holderName: "João",
+				cvv: "123",
+				expirationDate: "13/2099",
+				installments: 1,
+			});
+		}).toThrow("Invalid expiration month, must be between 1 and 12");
+	});
+
+	it("should throw an error if the card is expired", () => {
+		const pastYear = new Date().getFullYear() - 1;
+		expect(() => {
+			new Card({
+				number: "4111111111111111",
+				holderName: "João",
+				cvv: "123",
+				expirationDate: `01/${pastYear}`,
+				installments: 1,
+			});
+		}).toThrow("Card is expired");
+	});
+
+	it("should throw an error if the number of installments is less than 1", () => {
+		expect(() => {
+			new Card({
+				number: "4111111111111111",
+				holderName: "João",
+				cvv: "123",
+				expirationDate: "12/2099",
+				installments: 0,
+			});
+		}).toThrow("Invalid installments, must be greater than 0");
+	});
+
+	it("should return JSON correctly with all data", () => {
+		const card = new Card({
+			number: "4111111111111111",
+			holderName: "João da Silva",
+			cvv: "123",
+			expirationDate: "12/2099",
+			installments: 2,
+		});
+
+		expect(card.toJSON()).toEqual({
+			paymentType: "credit",
+			number: "4111111111111111",
+			holderName: "João da Silva",
+			cvv: "123",
+			expirationDate: "12/2099",
+			installments: 2,
+		});
+	});
+});
