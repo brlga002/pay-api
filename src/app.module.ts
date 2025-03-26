b11498c3114517b8af53cf3ffaf698155599e9a1
brlga002 <gabriel.lima1000@gmail.com>
2025-03-26 11:47:44 -0400

    feat: integrate ChargeController and payment services into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,37 @@ import { APP_INTERCEPTOR } from "@nestjs/core";
 import { RequestIdInterceptor } from "./infra/logger/request-id.interceptor";
 import { RequestContextService } from "./infra/logger/request-context.service";
 import { AppService } from "./app.service";
-import { MockProvider1Controller } from "@infra/controllers/mock-provider1.controller";
-import { MockProvider2Controller } from "@infra/controllers/mock-provider2.controller";
+import { ChargeController } from "@infra/controllers/charge.controller";
+import { ConfigModule } from "@nestjs/config";
+import { FallbackPaymentService } from "@infra/providers/fallback-payment.service";
+import { StripeService } from "@infra/providers/stripe.service";
+import { HttpModule } from "@nestjs/axios";
+import { ProcessPaymentUseCase } from "@application/use-cases/process-payment.use-case";
+import { PrismaModule } from "@infra/prisma/prisma.module";
+import { MockModule } from "@infra/mock-providers/mock.module";
 
 @Module({
-	controllers: [MockProvider1Controller, MockProvider2Controller],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		HttpModule,
+		PrismaModule,
+		MockModule,
+	],
+	controllers: [ChargeController],
 	providers: [
 		AppService,
 		RequestContextService,
+		StripeService,
+		ProcessPaymentUseCase,
 		{
 			provide: APP_INTERCEPTOR,
 			useClass: RequestIdInterceptor,
+		},
+		{
+			provide: FallbackPaymentService,
+			useFactory: (provide: StripeService) =>
+				new FallbackPaymentService([provide]),
+			inject: [StripeService],
 		},
 	],
 })
