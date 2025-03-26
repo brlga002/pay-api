558903735f0573e95a86b7ddd4dd4aeb34f01d7d
brlga002 <gabriel.lima1000@gmail.com>
2025-03-26 13:32:10 -0400

    feat: add CreateChargeUseCase for handling charge creation and payment processing

---
+++ src/application/use-cases/create-charge.use-case.ts
@@ -0,0 +1,46 @@
+import { Inject, Injectable, Logger } from "@nestjs/common";
+import { FallbackPaymentService } from "src/infra/providers/fallback-payment.service";
+import { Charge } from "@domain/entities/charge.entity";
+import { IChargeRepository } from "@domain/repositories/payment.repository";
+
+@Injectable()
+export class CreateChargeUseCase {
+	private readonly logger = new Logger(CreateChargeUseCase.name);
+
+	constructor(
+		@Inject("IChargeRepository")
+		private readonly ChargeRepository: IChargeRepository,
+		private readonly fallbackService: FallbackPaymentService,
+	) {}
+
+	async execute(charge: Charge): Promise<Charge> {
+		console.log(this.logger);
+		const existingPayment = await this.ChargeRepository.find(
+			charge.merchantId,
+			charge.orderId,
+		);
+
+		if (!existingPayment) {
+			this.logger.log(`Creating new charge ${charge.id}`);
+			await this.ChargeRepository.save(charge);
+		} else {
+			this.logger.log(`Charge ${existingPayment.id} already exists`);
+			if (!existingPayment.isReadyToProcess()) {
+				this.logger.warn(
+					`Charge ${existingPayment.id} is not ready to process`,
+				);
+				return existingPayment;
+			}
+		}
+
+		const response = await this.fallbackService.processPayment(charge);
+		charge.setProvider(response.provider);
+		charge.setPaymentSource({
+			id: response.cardId,
+			status: response.status,
+		});
+		await this.ChargeRepository.update(charge);
+
+		return charge;
+	}
+}
